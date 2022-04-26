0ed574dff3e61636c85612179f349f7095150810
ashmatta <ashish.matta1202@gmail.com>
2022-04-26 07:52:06 +0100

    created temp helper filex

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
+from multiprocessing import context
 from django.shortcuts import render
+from core.models import avgTemp
+import plotly.express as px
+
 
 # Create your views here.
+def chart(request):
+    temp = avgTemp.objects.all()
+
+    fig = px.line(
+        x=[t.date for t in avgTemp],
+        y=[t.average for t in avgTemp]
+    )
+
+    chart = fig.to_html()
+
+    context = {'chart': chart}
+    return render(request, 'core/chart.html', context)
